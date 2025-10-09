a5e73082225e01d80762e43fc905279638fed796
AlejandroTaba <tabagarciaalejandro@gmail.com>
2025-10-09 16:40:08 -0500

    añadir producto

--- views/pages/home/home.php
+++ views/pages/home/home.php
@@ -1,6 +1,8 @@
 <?php
 
     include "partials/slide.php";
-    include "partials/banner.php";
     include "partials/free.php";
+    include "partials/banner.php";
+    include "partials/most-seen.php";
+    include "partials/most-sold.php";
 ?>
--- views/pages/home/partials/most-seen.php
+++ views/pages/home/partials/most-seen.php
@@ -7,7 +7,7 @@
 
         <div class="btn-group float-end p-2">
 
-            <button class="btn btn-default btnView bg-white" attr-type="grid" attr-index="1">
+            <button class="btn btn-default btnView bg-info" attr-type="grid" attr-index="2">
 
                 <i class="fas fa-th fa-xs pe-1"></i>
 
@@ -18,7 +18,7 @@
 
  
 
-            <button class="btn btn-default btnView" attr-type="list" attr-index="1">
+            <button class="btn btn-default btnView" attr-type="list" attr-index="2">
 
                 <i class="fas fa-list fa-xs pe-1"></i>
 
@@ -36,7 +36,6 @@
 </div>
 
 
-
  
 
 <div class="container-fluid bg-white">
@@ -45,7 +44,7 @@
 
         <div class="clearfix pt-3 pb-0 px-2">
 
-            <h4 class="float-start text-uppercase pt-2">Artículos Gratuitos</h4>
+            <h4 class="float-start text-uppercase pt-2">Artículos más vistos</h4>
 
             <span class="float-end">
 
@@ -64,15 +63,16 @@
 
         <hr style="color:#555">
 
-
- 
+        
 
         <!-- GRID -->
 
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 pt-3 pb-4 grid-1">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 pt-3 pb-4 grid-2">
 
 
  
+
+            <!-- tarjeta 1 -->
 
             <div class="col px-3 py-2 py-lg-0">
 
@@ -80,14 +80,18 @@
 
                     <figure class="imgProduct">
 
-                        <img src="views/sources/img/products/accesorios/1/accesorio01.jpg" class="img-fluid" alt="">
+                        <img src="views/sources/img/products/ropa/1/ropa01.jpg" alt="ropa" class="img-fluid">
 
                     </figure>
 
 
  
 
-                    <h5><small class="text-uppercase text-muted">Collar de Diamantes</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Falda de Flores</small>
+
+                    </h5>
 
                 </a>
 
@@ -105,7 +109,11 @@
 
                 <div class="clearfix">
 
-                    <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                    <h5 class="float-start text-uppercase text-muted">
+
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+
+                    </h5>
 
 
  
@@ -140,20 +148,26 @@
 
  
 
+            <!-- tarjeta 2 -->
+
             <div class="col px-3 py-2 py-lg-0">
 
                 <a href="#">
 
                     <figure class="imgProduct">
 
-                        <img src="views/sources/img/products/accesorios/2/accesorio02.jpg" class="img-fluid" alt="">
+                        <img src="views/sources/img/products/ropa/2/ropa02.jpg" alt="ropa" class="img-fluid">
 
                     </figure>
 
 
  
 
-                    <h5><small class="text-uppercase text-muted">Bolso deportivo gris</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Vestido jean</small>
+
+                    </h5>
 
                 </a>
 
@@ -171,7 +185,11 @@
 
                 <div class="clearfix">
 
-                    <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                    <h5 class="float-start text-uppercase text-muted">
+
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+
+                    </h5>
 
 
  
@@ -206,20 +224,26 @@
 
  
 
+            <!-- tarjeta 3 -->
+
             <div class="col px-3 py-2 py-lg-0">
 
                 <a href="#">
 
                     <figure class="imgProduct">
 
-                        <img src="views/sources/img/products/accesorios/3/accesorio03.jpg" class="img-fluid" alt="">
+                        <img src="views/sources/img/products/ropa/3/ropa03.jpg" alt="ropa" class="img-fluid">
 
                     </figure>
 
 
  
 
-                    <h5><small class="text-uppercase text-muted">Bolso Militar</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Top Dama</small>
+
+                    </h5>
 
                 </a>
 
@@ -237,7 +261,11 @@
 
                 <div class="clearfix">
 
-                    <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                    <h5 class="float-start text-uppercase text-muted">
+
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+
+                    </h5>
 
 
  
@@ -269,8 +297,9 @@
 
             </div>
 
+            
 
- 
+            <!-- tarjeta 4 -->
 
             <div class="col px-3 py-2 py-lg-0">
 
@@ -278,14 +307,18 @@
 
                     <figure class="imgProduct">
 
-                        <img src="views/sources/img/products/accesorios/4/accesorio04.jpg" class="img-fluid" alt="">
+                        <img src="views/sources/img/products/ropa/4/ropa04.jpg" alt="ropa" class="img-fluid">
 
                     </figure>
 
 
  
 
-                    <h5><small class="text-uppercase text-muted">Pulsera de Diamantes</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Vestido Clásico</small>
+
+                    </h5>
 
                 </a>
 
@@ -303,7 +336,11 @@
 
                 <div class="clearfix">
 
-                    <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                    <h5 class="float-start text-uppercase text-muted">
+
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+
+                    </h5>
 
 
  
@@ -345,8 +382,7 @@
 
         <!-- LIST -->
 
-
-        <div class="row list-1" style="display:none">
+         <div class="row list-2" style="display:none">
 
 
  
@@ -355,7 +391,7 @@
 
                 <figure class="imgProduct">
 
-                    <img src="views/sources/img/products/accesorios/1/accesorio01.jpg" class="img-fluid" style="width:150px" alt="">
+                    <img src="views/sources/img/products/ropa/1/ropa01.jpg" alt="" style="width:150px">
 
                 </figure>
 
@@ -364,33 +400,45 @@
 
                 <div class="media-body ps-3">
 
-                    <h5><small class="text-uppercase text-muted">Collar de Diamantes</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Falda de Flores</small>
+
+                    </h5>
 
                     <span class="badge badgeNew bg-warning text-uppercase text-white p-2">Nuevo</span>
 
-                    <p class="my-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptas laborum suscipit amet perspiciatis, doloribus laudantium ea fuga exercitationem ipsa facere vel tenetur sit? Soluta!</p>
+                    <div class="my-2">
+
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, rerum! Sit maiores quia numquam repellendus, tempora suscipit cumque natus tempore autem distinctio cupiditate adipisci impedit?
+
+                    </div>
 
 
  
 
                     <div class="clearfix">
 
-                        <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                        <h5 class="float-start text-uppercase text-muted">
+
+                            <del class="small" style="color:#bbb">USD 129</del> $11
+
+                        </h5>
 
 
  
 
                         <span class="float-end">
 
-                            <div class="btn-group btn group-sm">
+                            <div class="btn-group btn-group-sm">
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-heart"></i>
 
                                 </button>
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-eye"></i>
 
@@ -404,9 +452,6 @@
 
                 </div>
 
-
- 
-
             </div>
 
 
@@ -416,7 +461,7 @@
 
                 <figure class="imgProduct">
 
-                    <img src="views/sources/img/products/accesorios/2/accesorio02.jpg" class="img-fluid" style="width:150px" alt="">
+                    <img src="views/sources/img/products/ropa/2/ropa02.jpg" alt="" style="width:150px">
 
                 </figure>
 
@@ -425,33 +470,45 @@
 
                 <div class="media-body ps-3">
 
-                    <h5><small class="text-uppercase text-muted">Bolso Deportivo Gris</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Falda de Flores</small>
+
+                    </h5>
 
                     <span class="badge badgeNew bg-warning text-uppercase text-white p-2">Nuevo</span>
 
-                    <p class="my-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptas laborum suscipit amet perspiciatis, doloribus laudantium ea fuga exercitationem ipsa facere vel tenetur sit? Soluta!</p>
+                    <div class="my-2">
+
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, rerum! Sit maiores quia numquam repellendus, tempora suscipit cumque natus tempore autem distinctio cupiditate adipisci impedit?
+
+                    </div>
 
 
  
 
                     <div class="clearfix">
 
-                        <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                        <h5 class="float-start text-uppercase text-muted">
+
+                            <del class="small" style="color:#bbb">USD 129</del> $11
+
+                        </h5>
 
 
  
 
                         <span class="float-end">
 
-                            <div class="btn-group btn group-sm">
+                            <div class="btn-group btn-group-sm">
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-heart"></i>
 
                                 </button>
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-eye"></i>
 
@@ -465,9 +522,6 @@
 
                 </div>
 
-
- 
-
             </div>
 
 
@@ -477,7 +531,7 @@
 
                 <figure class="imgProduct">
 
-                    <img src="views/sources/img/products/accesorios/3/accesorio03.jpg" class="img-fluid" style="width:150px" alt="">
+                    <img src="views/sources/img/products/ropa/3/ropa03.jpg" alt="" style="width:150px">
 
                 </figure>
 
@@ -486,33 +540,45 @@
 
                 <div class="media-body ps-3">
 
-                    <h5><small class="text-uppercase text-muted">Bolso Militar</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Falda de Flores</small>
+
+                    </h5>
 
                     <span class="badge badgeNew bg-warning text-uppercase text-white p-2">Nuevo</span>
 
-                    <p class="my-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptas laborum suscipit amet perspiciatis, doloribus laudantium ea fuga exercitationem ipsa facere vel tenetur sit? Soluta!</p>
+                    <div class="my-2">
+
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, rerum! Sit maiores quia numquam repellendus, tempora suscipit cumque natus tempore autem distinctio cupiditate adipisci impedit?
+
+                    </div>
 
 
  
 
                     <div class="clearfix">
 
-                        <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                        <h5 class="float-start text-uppercase text-muted">
+
+                            <del class="small" style="color:#bbb">USD 129</del> $11
+
+                        </h5>
 
 
  
 
                         <span class="float-end">
 
-                            <div class="btn-group btn group-sm">
+                            <div class="btn-group btn-group-sm">
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-heart"></i>
 
                                 </button>
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-eye"></i>
 
@@ -526,9 +592,6 @@
 
                 </div>
 
-
- 
-
             </div>
 
 
@@ -538,7 +601,7 @@
 
                 <figure class="imgProduct">
 
-                    <img src="views/sources/img/products/accesorios/4/accesorio04.jpg" class="img-fluid" style="width:150px" alt="">
+                    <img src="views/sources/img/products/ropa/4/ropa04.jpg" alt="" style="width:150px">
 
                 </figure>
 
@@ -547,33 +610,45 @@
 
                 <div class="media-body ps-3">
 
-                    <h5><small class="text-uppercase text-muted">pulsera de Diamantes</small></h5>
+                    <h5>
+
+                        <small class="text-uppercase text-muted">Falda de Flores</small>
+
+                    </h5>
 
                     <span class="badge badgeNew bg-warning text-uppercase text-white p-2">Nuevo</span>
 
-                    <p class="my-2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptas laborum suscipit amet perspiciatis, doloribus laudantium ea fuga exercitationem ipsa facere vel tenetur sit? Soluta!</p>
+                    <div class="my-2">
+
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, rerum! Sit maiores quia numquam repellendus, tempora suscipit cumque natus tempore autem distinctio cupiditate adipisci impedit?
+
+                    </div>
 
 
  
 
                     <div class="clearfix">
 
-                        <h5 class="float-start text-uppercase text-muted"><small>Gratis</small></h5>
+                        <h5 class="float-start text-uppercase text-muted">
+
+                            <del class="small" style="color:#bbb">USD 129</del> $11
+
+                        </h5>
 
 
  
 
                         <span class="float-end">
 
-                            <div class="btn-group btn group-sm">
+                            <div class="btn-group btn-group-sm">
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-heart"></i>
 
                                 </button>
 
-                                <button type="button" class="btn btn-light border">
+                                <button type="button" class="btn btn light border">
 
                                     <i class="fas fa-eye"></i>
 
@@ -587,18 +662,13 @@
 
                 </div>
 
-
- 
-
             </div>
 
-            
-
-
- 
+         
 
         </div>
 
+        
 
 
 
@@ -608,4 +678,3 @@
     </div>
 
 </div>
-
